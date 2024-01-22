760ea4f9553ff130fc2abb87adb1aec1baab116e
Nadir KHOUANI <khouani.nadir7@gmail.com>
2024-01-22 10:36:04 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -6,5 +6,8 @@ const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUserById);
+
 
 module.exports = app;
